7b7e24385684611ef3d78cb70ff23955eb1d01e7
daltonmitchell <dzack34@gmail.com>
2022-06-20 14:14:56 -0700

    displays submitted flights on the all flights page

--- routes/flights.js
+++ routes/flights.js
@@ -3,5 +3,7 @@ var router = express.Router();
 const flightsCtrl = require('../controllers/flights')
 
 router.get('/new', flightsCtrl.new);
+router.post('/', flightsCtrl.create);
+router.get('/', flightsCtrl.index);
 
 module.exports = router;
